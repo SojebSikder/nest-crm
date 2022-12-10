7197e9db6da3b6377bd3adc7cfe5fcb44fa08377
sojebsikder <sojebsikder@gmail.com>
2022-12-10 12:01:45 +0600

    main

--- src/app/whatsapp/whatsapp.controller.ts
+++ src/app/whatsapp/whatsapp.controller.ts
@@ -218,36 +218,40 @@ export class WhatsappController {
   // public api
   @Get('webhook/:webhook_key')
   async webhookGet(@Request() req, @Response() res) {
-    const webhook_key = req.params.webhook_key;
-    const workspaceChannel = await this.whatsappService.findOne(webhook_key);
-    /**
-     * UPDATE YOUR VERIFY TOKEN
-     *This will be the Verify Token value when you set up webhook
-     **/
-    let verify_token = null;
-    if (workspaceChannel) {
-      verify_token = workspaceChannel.verify_token;
-    } else {
-      // Return a '404 Not Found' if event is not from a WhatsApp API
-      res.sendStatus(404);
-    }
-
-    // Parse params from the webhook verification request
-    const mode = req.query['hub.mode'];
-    const token = req.query['hub.verify_token'];
-    const challenge = req.query['hub.challenge'];
-
-    // Check if a token and mode were sent
-    if (mode && token) {
-      // Check the mode and token sent are correct
-      if (mode === 'subscribe' && token === verify_token) {
-        // Respond with 200 OK and challenge token from the request
-        console.log('WEBHOOK_VERIFIED');
-        res.status(200).send(challenge);
+    try {
+      const webhook_key = req.params.webhook_key;
+      const workspaceChannel = await this.whatsappService.findOne(webhook_key);
+      /**
+       * UPDATE YOUR VERIFY TOKEN
+       *This will be the Verify Token value when you set up webhook
+       **/
+      let verify_token = null;
+      if (workspaceChannel) {
+        verify_token = workspaceChannel.verify_token;
       } else {
-        // Responds with '403 Forbidden' if verify tokens do not match
-        res.sendStatus(403);
+        // Return a '404 Not Found' if event is not from a WhatsApp API
+        res.sendStatus(404);
       }
+
+      // Parse params from the webhook verification request
+      const mode = req.query['hub.mode'];
+      const token = req.query['hub.verify_token'];
+      const challenge = req.query['hub.challenge'];
+
+      // Check if a token and mode were sent
+      if (mode && token) {
+        // Check the mode and token sent are correct
+        if (mode === 'subscribe' && token === verify_token) {
+          // Respond with 200 OK and challenge token from the request
+          console.log('WEBHOOK_VERIFIED');
+          res.status(200).send(challenge);
+        } else {
+          // Responds with '403 Forbidden' if verify tokens do not match
+          res.sendStatus(403);
+        }
+      }
+    } catch (error) {
+      throw error;
     }
   }
 }
