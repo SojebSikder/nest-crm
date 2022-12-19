a9cbe4827b5634408a1018f78b9128f5e0974fbd
sojebsikder <sojebsikder@gmail.com>
2022-12-19 12:33:14 +0600

    added demo kvstore

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,36 @@
 import { Controller, Request, Get, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
 import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
+import { KvStore } from './common/lib/KvStore';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Get('test')
+  async test() {
+    KvStore.connect({ host: 'http://localhost:8080' });
+    const jsonData = {
+      name: 'sojeb',
+      age: 20,
+    };
+    const jsonDataArr = [
+      {
+        name: 'sojeb',
+        age: 20,
+      },
+      {
+        name: 'sikder',
+        age: 18,
+      },
+    ];
+
+    await KvStore.set('json', jsonData);
+    await KvStore.set('jsonArray', jsonDataArr);
+    const result = await KvStore.get('json');
+    const resultArr = await KvStore.get('jsonArray');
+    return { json: result, arr: resultArr };
+  }
 
   @Get()
   getHello(): string {
