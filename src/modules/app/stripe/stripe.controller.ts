e1afa9bc545c375f2e6c64656aa7c117de21aaa9
sojebsikder <sojebsikder@gmail.com>
2023-02-20 21:23:13 +0600

    main

--- src/modules/app/stripe/stripe.controller.ts
+++ src/modules/app/stripe/stripe.controller.ts
@@ -1,24 +1,17 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-  Req,
-  Res,
-} from '@nestjs/common';
+import { Controller, Post, Req, Res } from '@nestjs/common';
 import { StripeService } from './stripe.service';
 import { StripeMethod } from '../../../common/lib/Payment/stripe/Stripe';
 import { Request, Response } from 'express';
 import Stripe from 'stripe';
 import { UserRepository } from '../../../common/repository/user/user.repository';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Stripe')
 @Controller('stripe')
 export class StripeController {
   constructor(private readonly stripeService: StripeService) {}
 
+  @ApiOperation({ summary: 'webhook' })
   @Post('webhook')
   async create(@Req() req: Request, @Res() res: Response) {
     try {
